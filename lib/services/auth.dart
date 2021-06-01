5d7cea9f34d9d6c0e32e01b10b032d5483d94a51
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-01 13:23:49 +0500

    The authStateChanges stream

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,10 +3,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 abstract class AuthBase {
   User get currentUser;
   Future<User> signInAnonymously();
+  Stream<User> authStateChanges();
   Future<void> signOut();
 }
 
 class Auth implements AuthBase {
+  @override
+  Stream<User> authStateChanges() => _firebaseAuth.authStateChanges();
   final _firebaseAuth = FirebaseAuth.instance;
   @override
   User get currentUser => _firebaseAuth.currentUser;
