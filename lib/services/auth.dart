6ff56eb012c0d7000b6a8918305cc2b8f1590de2
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-06 12:53:22 +0500

    Adding Google Sign-In to the Auth class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User get currentUser;
   Future<User> signInAnonymously();
   Stream<User> authStateChanges();
+  Future<User> signInWithGoogle();
   Future<void> signOut();
 }
 
@@ -18,6 +20,28 @@ class Auth implements AuthBase {
   Future<User> signInAnonymously() async {
     final userCredential = await _firebaseAuth.signInAnonymously();
     return userCredential.user;
+  }
+   @override
+  Future<User> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.credential(
+              idToken: googleAuth.idToken, accessToken: googleAuth.accessToken),
+        );
+        return userCredential.user;
+      } else {
+        throw FirebaseAuthException(
+            message: "Missing Google Token",
+            code: "ERROR_MISSING_GOOGLE_ID_TOKEN");
+      }
+    } else {
+      throw FirebaseAuthException(
+          message: "Sign is aborted by user", code: "ERROR_ABORTED BY USER");
+    }
   }
 
   @override
