65b46254206ee71c69cb3a3115cf3c5a0879f961
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-09 02:09:47 +0500

    Adding the email and password text fields

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration:
+            InputDecoration(labelText: "Email", hintText: "test@gmail.com"),
+      ),
+      TextField(
+        decoration: InputDecoration(
+          hintText: "Password",
+        ),
+        obscureText: true,
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
   }
 }
--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -6,7 +6,7 @@ class EmailSignInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Time Tracker'),
+        title: Text('Sign In'),
         elevation: 10.0,
       ),
       body: Card(
