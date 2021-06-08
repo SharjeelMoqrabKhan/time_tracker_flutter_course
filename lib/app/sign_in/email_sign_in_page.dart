1b0c72b1fb4296f4f2c4782787c24703be3cec54
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-09 01:02:47 +0500

    Creating the email & password sign-in page

---
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class EmailSignInPage extends StatelessWidget {
+ 
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 10.0,
+      ),
+      body: _buildContent(),
+            backgroundColor: Colors.grey[200],
+          );
+        }
+      
+        Widget _buildContent() {
+          return Container(
+            
+          );
+        }
+  
+}
