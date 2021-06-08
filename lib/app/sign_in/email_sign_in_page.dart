99e1013d0529e743abfacd7aece852215fb6bbca
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-09 01:38:21 +0500

    Adding a Card widget

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_form.dart';
 
 class EmailSignInPage extends StatelessWidget {
- 
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
         elevation: 10.0,
       ),
-      body: _buildContent(),
-            backgroundColor: Colors.grey[200],
-          );
-        }
-      
-        Widget _buildContent() {
-          return Container(
-            
-          );
-        }
-  
+      body: Card(
+        child: EmailSignInForm(),
+      ),
+      backgroundColor: Colors.grey[200],
+    );
+  }
 }
