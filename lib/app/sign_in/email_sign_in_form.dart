b95e928f753c8b1cc416c3b04b4905f8cc9000a5
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-09 13:38:36 +0500

    Sizedbox added for spacing between textfeilds

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_course/app/sign_in/sign_In_button.dart';
 import 'package:time_tracker_flutter_course/common_widgets/custom_sign_in_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
@@ -9,15 +8,24 @@ class EmailSignInForm extends StatelessWidget {
         decoration:
             InputDecoration(labelText: "Email", hintText: "test@gmail.com"),
       ),
+      SizedBox(
+        height: 10,
+      ),
       TextField(
         decoration: InputDecoration(
           hintText: "Password",
         ),
         obscureText: true,
       ),
+      SizedBox(
+        height: 10,
+      ),
       FormSubmitButton(
         text: "Sign In",
         onPressed: () {},
+      ),
+      SizedBox(
+        height: 10,
       ),
       FlatButton(
         onPressed: () {},
