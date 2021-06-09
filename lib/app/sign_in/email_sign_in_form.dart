8a4eb491364972c7fa69b4e5747f7b6b33936b90
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-09 15:39:19 +0500

    Adding a TextEditingController

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/common_widgets/custom_sign_in_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _onSubmit() {
+    print(
+        "Email: ${_emailController.text} Password ${_passwordController.text}");
+  }
+
   List<Widget> _buildChildren() {
     return [
       TextField(
         decoration:
             InputDecoration(labelText: "Email", hintText: "test@gmail.com"),
+        controller: _emailController,
       ),
       SizedBox(
         height: 10,
@@ -15,6 +24,7 @@ class EmailSignInForm extends StatelessWidget {
         decoration: InputDecoration(
           hintText: "Password",
         ),
+        controller: _passwordController,
         obscureText: true,
       ),
       SizedBox(
@@ -22,7 +32,7 @@ class EmailSignInForm extends StatelessWidget {
       ),
       FormSubmitButton(
         text: "Sign In",
-        onPressed: () {},
+        onPressed: _onSubmit,
       ),
       SizedBox(
         height: 10,
@@ -38,10 +48,12 @@ class EmailSignInForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: _buildChildren(),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: _buildChildren(),
+        ),
       ),
     );
   }
