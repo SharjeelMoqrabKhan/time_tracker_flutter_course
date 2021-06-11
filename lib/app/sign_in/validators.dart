54bec118c3f929e52f28d31c0f1533da479bc1e8
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-11 23:52:00 +0500

    Adding a StringValidator class

---
+++ lib/app/sign_in/validators.dart
@@ -0,0 +1,10 @@
+abstract class StringValidator {
+  bool isValid(String value);
+}
+
+class NonEmptyStringValidator implements StringValidator {
+  @override
+  bool isValid(String value) {
+    return value.isEmpty;
+  }
+}
