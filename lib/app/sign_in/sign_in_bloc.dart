8de3722f531288ec756fe6b0cff28b89ca2c4dc2
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-06-25 11:21:10 +0500

    State Management with Provider

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -4,24 +4,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class SignInBloc {
-  SignInBloc({@required this.auth});
+  SignInBloc({@required this.auth,@required this.isLoading});
   final AuthBase auth;
+   final ValueNotifier<bool> isLoading;
 
-  final StreamController<bool> _isLoadingController = StreamController<bool>();
-
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
-
-  void dispose() {
-    _isLoadingController.close();
-  }
-
-  void _setLoading(bool isLoading) => _isLoadingController.add(isLoading);
   Future<User> _signIn(Future<User> Function() signInMethod) async {
     try {
-      _setLoading(true);
+      isLoading.value=true;
       return await signInMethod();
     } catch (e) {
-      _setLoading(false);
+      isLoading.value=false;
       rethrow;
     }
   }
