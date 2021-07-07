ef60dda63ad9e8516d25b5d8d61efa64164e40b3
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-07-07 15:17:10 +0500

    Adding swipe to delete support

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs/edit_jobs.dart';
@@ -5,6 +6,7 @@ import 'package:time_tracker_flutter_course/app/home/jobs/job_tile.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs/list_item_builder.dart';
 import 'package:time_tracker_flutter_course/app/home/model/job.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/common_widgets/show_firebase_atuh_exception.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 import 'package:time_tracker_flutter_course/services/database.dart';
 
@@ -26,6 +28,15 @@ class JobsPage extends StatelessWidget {
         cancleActionText: "Cancle");
     if (didSignOutRequest == true) {
       _signOut(context);
+    }
+  }
+
+  Future<void> _delete(BuildContext context, Job job) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.deleteJob(job);
+    } on FirebaseException catch (e) {
+      showExceptionAlertBox(context, title: 'Operation Failed', exception: e);
     }
   }
 
@@ -62,9 +73,15 @@ class JobsPage extends StatelessWidget {
       builder: (context, snapshot) {
         return ListItemBuilder<Job>(
           snapshot: snapshot,
-          itemBuilder: (context, job) => JobTile(
-            job: job,
-            onTap: () => EditJobPage.show(context, job: job),
+          itemBuilder: (context, job) => Dismissible(
+            key: Key('job-${job.id}'),
+            background: Container(color: Colors.red),
+            direction: DismissDirection.startToEnd,
+            onDismissed: (dirction) => _delete(context, job),
+            child: JobTile(
+              job: job,
+              onTap: () => EditJobPage.show(context, job: job),
+            ),
           ),
         );
       },
